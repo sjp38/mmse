7b10f1c6e3ded964070096a8a15e13b74d569285
Yunjae Lee <lyj7694@gmail.com>
2018-11-14 08:46:14 +0000

    dap_trans: Implement blktrace-styled dap translator
    This commit implements blktrace-styled dap translator and refines
    argument options works with multiple dap styles.
    
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- dap_trans.py
+++ dap_trans.py
@@ -3,6 +3,8 @@
 import argparse
 import re
 
+SECTOR_SIZE = 512
+
 def get_obj_info_daphic(obj_path):
     obj_size = {}
     obj_addr = {}
@@ -49,6 +51,36 @@ def dap_trans_daphic(obj_path, dap_path):
         daps.append(dap)
     return daps
 
+def dap_trans_blktrace(dap_path):
+    daps = []
+    dap = []
+    with open(dap_path, 'r') as org_dap:
+        for line in org_dap:
+            if line.startswith('CPU'):
+                break
+            if line.startswith('#'):
+                continue
+            fields = re.split(', ', line.lstrip().rstrip())
+            if fields[3] != 'C':
+                continue
+            time = float(fields[0])
+            pid = int(fields[1])
+            cmd = fields[2]
+            sector_id = int(fields[4])
+            acc_size = int(fields[5])
+            sector_addr = sector_id * SECTOR_SIZE
+            addr = [sector_addr, sector_addr + acc_size - 1]
+            dap.append(str(addr[0]) + '-' + str(addr[1]))
+            dap.append("sequential")
+            dap.append("4")
+            dap.append(str(acc_size//4))
+            dap.append('\n')
+
+            daps.append('# time %.6fs,\tpid %d\n' % (time, pid))
+            daps.append(dap)
+            dap = []
+    return daps
+
 def write_out_daps(output_path, daps):
     with open(output_path, 'w') as mmse_dap:
         for dap in daps:
@@ -66,24 +98,34 @@ def write_out_daps(output_path, daps):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    subparsers_style = parser.add_subparsers(help = 'choose source dap style')
+    subparser_daphic = subparsers_style.add_parser('daphic')
+    subparser_blktrace = subparsers_style.add_parser('blktrace')
+    
+#   daphic
+    subparser_daphic.add_argument('src_dap', type = str, 
+            help='path for source dap')
+    subparser_daphic.add_argument('obj', type = str,
+            help = 'path for object info')
+    subparser_daphic.add_argument('dst_dap', type = str,
+            help = 'path to write out translated dap')
+    subparser_daphic.set_defaults(src_style = 'daphic')
 
-    parser.add_argument('--style', choices=['daphic'], default='daphic',
-            metavar='dap_style', help='in which style is this dap written?')
-    parser.add_argument('--indap', type=str,
-            default='daps/others/daphic/470.lbm.dap.dat', metavar='input_dap',
-            help='path for input dap')
-    parser.add_argument('--inobj', type=str,
-            default='daps/others/daphic/470.lbm.alloctrace',
-            metavar='input_obj', help='path for object info')
-    parser.add_argument('--outdap', type=str, default='daps/daphic/470.lbm',
-            metavar='output_dap', help='path to write out translated dap')
-
+#   blktrace
+    subparser_blktrace.add_argument('src_dap', type = str,
+            help = 'path for source dap')
+    subparser_blktrace.add_argument('dst_dap', type = str,
+            help = 'path to write out translated dap')
+    subparser_blktrace.set_defaults(src_style = 'blktrace')    
+    
     args = parser.parse_args()
-
-    dap_path = args.indap
-    obj_path = args.inobj
-    output_path = args.outdap
-    dap_style = args.style
-
-    daps = dap_trans_daphic(obj_path, dap_path)
-    write_out_daps(output_path, daps)
+    src_path = args.src_dap
+    dst_path = args.dst_dap
+    src_style = args.src_style
+    
+    if src_style == 'daphic':
+        obj_path = args.obj
+	daps = dap_trans_daphic(obj_path, src_path)
+    elif src_style == 'blktrace':
+	daps = dap_trans_blktrace(src_path)
+    write_out_daps(dst_path, daps)
